15757812c4de3deff677f12c91a52b5ba0933267
zheng2x2 <zheng@>
2021-01-22 17:48:46 +0900

    add three.js case

---
+++ threejs/js/index.js
@@ -0,0 +1,27 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+// renderer.setSize( 544, 544 );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material ); 
+scene.add( cube );
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+
+const animate = function () {
+    requestAnimationFrame( animate );
+
+    // cube.rotation.x += 0.05;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
+
